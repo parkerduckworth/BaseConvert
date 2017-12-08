44b61704cf008b525a48eeec77d546937448a8fa
parkerduckworth <parkerduckworth@gmail.com>
2017-12-08 13:48:38 -0800

    tabs and spaces

--- base_convert.py
+++ base_convert.py
@@ -24,75 +24,75 @@ print(single_line)
 
 # Main body of program
 def main(number, base):
-	sequence = []
+    sequence = []
 
-	if base == 16:
-		hex(number, base)
-	else:
-		for i in range(int(len(str(number)) / 0.301) + 1):  # k = log10(2) ≈ 0.301
-			digit = get_remainder(number, base)
-			sequence.append(digit)
-			number = get_quotient(number, base)
-	
-		result = ''.join(reversed([str(x) for x in sequence]))
-		print("Result: {}".format(result.lstrip('0')))
-		print(single_line)
-		prog_repeat()
+    if base == 16:
+        hex(number, base)
+    else:
+        for i in range(int(len(str(number)) / 0.301) + 1):  # k = log10(2) ≈ 0.301
+            digit = get_remainder(number, base)
+            sequence.append(digit)
+            number = get_quotient(number, base)
+    
+        result = ''.join(reversed([str(x) for x in sequence]))
+        print("Result: {}".format(result.lstrip('0')))
+        print(single_line)
+        prog_repeat()
 
 
 # Handles hexadecimal integers
 def hex(number, base):
-	sequence = []
-	char_num_map = {'10': 'A', '11': 'B', '12': 'C', '13': 'D', '14': 'E', '15': 'F'}
+    sequence = []
+    char_num_map = {'10': 'A', '11': 'B', '12': 'C', '13': 'D', '14': 'E', '15': 'F'}
 
-	for i in range(int(len(str(number)) / 0.301) + 1):  # k = log10(2) ≈ 0.301
-		digit = str(get_remainder(number, base))
+    for i in range(int(len(str(number)) / 0.301) + 1):  # k = log10(2) ≈ 0.301
+        digit = str(get_remainder(number, base))
 
-		if int(digit) > 10:
-			digit = char_num_map[digit]
+        if int(digit) > 10:
+            digit = char_num_map[digit]
 
-		sequence.append(digit)
-		number = get_quotient(number, base)
-	
-	result = ''.join(reversed([str(x) for x in sequence]))
-	print("Result: {}".format(result.lstrip('0')))
-	print(single_line)
-	prog_repeat()
-	
+        sequence.append(digit)
+        number = get_quotient(number, base)
+    
+    result = ''.join(reversed([str(x) for x in sequence]))
+    print("Result: {}".format(result.lstrip('0')))
+    print(single_line)
+    prog_repeat()
+    
 
 # Allows for repeated sessions
 def prog_repeat():
-	recurse = input("Convert another integer? (Y/N): ")
-	print(single_line)
+    recurse = input("Convert another integer? (Y/N): ")
+    print(single_line)
 
-	if recurse.upper() == 'Y':
-		number = int(input("Enter number to convert: "))
-		print(single_line)
-		base = int(input("Enter base of conversion: "))
-		print(single_line)
+    if recurse.upper() == 'Y':
+        number = int(input("Enter number to convert: "))
+        print(single_line)
+        base = int(input("Enter base of conversion: "))
+        print(single_line)
 
-		if base == 16:
-			hex(number, base)
-		else:
-			main(number, base)
+        if base == 16:
+            hex(number, base)
+        else:
+            main(number, base)
 
-	elif recurse.upper() == 'N':
-		print("Goodbye!")
-	else:
-		print("Not a valid input.")
-		prog_repeat()
+    elif recurse.upper() == 'N':
+        print("Goodbye!")
+    else:
+        print("Not a valid input.")
+        prog_repeat()
 
 
 # Calculates quotient to be used as next coefficient
 def get_quotient(dividend, divisor):
-	quotient = dividend // divisor
-	return quotient
+    quotient = dividend // divisor
+    return quotient
 
 
 # Calculates remainder used as digit in result sequence
 def get_remainder(dividend, divisor):
-	remainder = dividend % divisor
-	return remainder
+    remainder = dividend % divisor
+    return remainder
 
 
 
